e4f6609e2b0a16cc03c0649584d5568490f8cc1c
purin273 <purin.gosai@gmail.com>
2021-04-13 08:27:59 +0530

    initalize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+  apiKey: "AIzaSyBoHDo27qLCDYEQZW8GP8oNMbOHn0YMBPs",
+  authDomain: "chat-web-app-f95c7.firebaseapp.com",
+  projectId: "chat-web-app-f95c7",
+  storageBucket: "chat-web-app-f95c7.appspot.com",
+  messagingSenderId: "807426671926",
+  appId: "1:807426671926:web:a34e2eb547c20bd673df97"
+};
+
+const app =firebase.initializeApp(config);
